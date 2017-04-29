858b88301c907acbc726ce822b85553d2be9ad23
CHIEN PO LIN <chienpolin@CHIENde-MacBook-Air.local>
2017-04-30 03:03:26 +0800

    fix homepage

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -16,8 +16,11 @@ class homeController
 
     function run()
     {
+        $mysql_fake_data = json_decode("[ { \"pro_no\": \"C0001\", \"pro_name\": \"卡涼多功能寵物墊 散熱墊 椅墊(小型寵用)\", \"pro_pic\": \"C0001.jpg\", \"pro_price\": \"500\", \"pro_score\": 4 }, { \"pro_no\": \"C0002\", \"pro_name\": \"日本IRIS《除貓砂踏墊》NO-550\", \"pro_pic\": \"C0002.jpg\", \"pro_price\": \"699\", \"pro_score\": 5 } ]",true);
+
+//        die(var_dump($mysql_fake_data));
         $db = getDB();
-        $this->response = $this->view->render($this->response, 'index.twig');
+        $this->response = $this->view->render($this->response, 'index.twig',['data'=>$mysql_fake_data]);
         return $this->response;
     }
 }
--- lib/mysql.php
+++ lib/mysql.php
@@ -5,22 +5,13 @@ use Medoo\Medoo;
 // Initialize
 function getDB()
 {
-    $locallist = array(
-        '127.0.0.1',
-        '::1'
-    );
-
-    if (!in_array($_SERVER['REMOTE_ADDR'], $locallist)) {
-        //setting production db connect
-    } else {
-        return new Medoo([
-            'database_type' => 'mysql',
-            'database_name' => 'foodie',
-            'server' => 'localhost',
-            'username' => 'root',
-            'password' => 'fzwV9d4G',
-            'charset' => 'utf8'
-        ]);
-    }
-
+    return new Medoo([
+        'database_type' => 'mysql',
+        'database_name' => 'foodie',
+        'server' => '127.0.0.1',
+        'username' => 'root',
+        'password' => 'root',
+        'socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
+        'charset' => 'utf8',
+    ]);
 }
