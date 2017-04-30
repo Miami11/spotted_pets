265130ee4e09bfd364025595926324654027cc92
mia chen <ya.ting.chen110@gmail.com>
2017-05-01 00:09:47 +0800

    fix bugs

--- controllers/homeController.php
+++ controllers/homeController.php
@@ -16,8 +16,12 @@ class homeController
 
     function run()
     {
-        $db = getDB();
-        $this->response = $this->view->render($this->response, 'index.twig');
+        $json = "{ \"product_cat\": [ { \"pro_no\": \"cat10001\", \"pro_name\": \"貓砂盆\", \"pro_info\": \"給貓咪最好的禮物\", \"pro_score\": 1, \"pro_pic\": \"product1\" }, { \"pro_no\": \"cat10001\", \"pro_name\": \"貓砂盆\", \"pro_info\": \"給貓咪最好的禮物\", \"pro_score\": 1, \"pro_pic\": \"product1\" } ] }";
+
+        $json = json_decode($json,true)['product_cat'];
+        //$db = getDB();
+
+        $this->response = $this->view->render($this->response, 'index.twig' ,['data'=>$json]);
         return $this->response;
     }
 }
